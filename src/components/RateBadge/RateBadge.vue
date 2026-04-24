624e6fb9ffb4e9816f646211fc1fe64a9e05e374
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-24 21:43:42 +0200

    fix: add aria-label to RateBadge for improved accessibility

--- src/components/RateBadge/RateBadge.vue
+++ src/components/RateBadge/RateBadge.vue
@@ -5,9 +5,8 @@ const props = defineProps<{ rate: number | null }>();
 <template>
   <div
     class="w-fit flex items-center gap-1 bg-black/50 text-white text-xs px-1.5 py-0.5 rounded"
-    aria-label="show rating"
   >
-    <span v-if="props.rate !== null">
+    <span v-if="props.rate !== null" :aria-label="`rating ${props.rate}`">
       {{ props.rate }}
     </span>
     <span v-else aria-label="Rating Is Not Available">-</span>
