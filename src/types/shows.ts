2eb6dc9c5553155fb5eecf9193b29420aa386bf0
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-24 20:51:59 +0200

    feat: add Show interface and related types for show details management

---
+++ src/types/shows.ts
@@ -0,0 +1,19 @@
+interface Image {
+  medium: string;
+  original: string;
+}
+
+interface Rating {
+  average: number | null;
+}
+
+export interface Show {
+  id: number;
+  name: string;
+  type: string;
+  language: string;
+  genres: string[];
+  rating: Rating;
+  image: Image | null;
+  summary: string | null;
+}
