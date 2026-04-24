4e4f3d95a8588e787732b24e394603eb1336b613
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-24 11:41:38 +0200

    refactor: simplify App.vue layout and remove unused assets and components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue';
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <HelloWorld />
+  <main>
+    <h1>tv shows</h1>
+  </main>
 </template>
