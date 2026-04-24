641ff45f9472e182cf86b23e0375d88ae20bc2f3
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-24 19:24:38 +0200

    feat: create RateBadge component to display ratings with StarIcon

---
+++ src/components/RateBadge/RateBadge.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import StarIcon from '../icons/StarIcon.vue';
+const props = defineProps<{ rate: number | null }>();
+</script>
+<template>
+  <div
+    class="w-fit flex items-center gap-1 bg-black/50 text-white text-xs px-1.5 py-0.5 rounded"
+    aria-label="show rating"
+  >
+    <span v-if="props.rate !== null">
+      {{ props.rate }}
+    </span>
+    <span v-else aria-label="Rating Is Not Available">-</span>
+    <StarIcon />
+  </div>
+</template>
