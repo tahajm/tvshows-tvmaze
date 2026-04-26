44813c38a14c8823df6044dfcf5ed7a1e71b4a18
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-26 10:41:52 +0200

    style: enhance genre heading in ShowsList view with bold font and increased size

--- src/views/ShowsList.vue
+++ src/views/ShowsList.vue
@@ -3,7 +3,6 @@ import ShowCard from '@/components/ShowCard.vue';
 import { useShows } from '@/composables/useShows';
 
 const { isLoading, showsByGenre } = useShows();
-
 </script>
 <template>
   <main class="px-4 py-8">
@@ -12,7 +11,7 @@ const { isLoading, showsByGenre } = useShows();
       <div v-if="isLoading">Loading...</div>
       <ul v-else>
         <li class="py-8" v-for="(shows, genre) in showsByGenre" :key="genre">
-          <h1 class="py-4">{{ genre }}</h1>
+          <h1 class="py-4 font-bold text-2xl">{{ genre }}</h1>
           <ol class="flex py-4 gap-4 overflow-x-auto snap-x">
             <li v-for="show in shows" :key="show.id" class="snap-start">
               <ShowCard :show="show" />
