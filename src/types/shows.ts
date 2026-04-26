b14cd6368b06334513ea0a8bdf3a679193e2322b
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-26 19:45:53 +0200

    feat: extend Show interface to include average runtime, network details, and embedded episodes, cast, and images for comprehensive show data representation

--- src/types/shows.ts
+++ src/types/shows.ts
@@ -7,6 +7,45 @@ interface Rating {
   average: number | null;
 }
 
+interface Resolution {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface ImageFull {
+  id: number;
+  type: 'poster' | 'background';
+  resolutions: {
+    original: Resolution;
+    medium: Resolution;
+  };
+}
+
+export interface Cast {
+  person: {
+    id: number;
+    name: string;
+    image?: Image;
+  };
+  character: {
+    id: number;
+    name: string;
+    image?: Image;
+  };
+}
+export interface Episode {
+  id: number;
+  name: string;
+  airdate: string;
+  runtime: number;
+  rating: Rating;
+  image: Image;
+  summary: string;
+  season: number;
+  number: number;
+}
+
 export interface Show {
   id: number;
   name: string;
@@ -17,4 +56,13 @@ export interface Show {
   image: Image | null;
   summary: string | null;
   status: 'Running' | 'Ended';
+  averageRuntime: number;
+  network: {
+    name: string;
+  };
+  _embedded?: {
+    episodes?: Episode[];
+    cast?: Cast[];
+    images?: ImageFull[];
+  };
 }
