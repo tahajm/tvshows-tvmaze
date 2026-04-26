95487befc7952373714776b10ca20eea5cfd8aa5
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-26 21:31:55 +0200

    feat: integrate AppHeader and AppFooter components for improved layout and structure

---
+++ src/components/AooFooter.vue
@@ -0,0 +1,7 @@
+<template>
+  <footer class="mt-auto bg-primary text-white/70">
+    <div class="py-4 text-center text-xs">
+      {{ new Date().getFullYear() }} TV Shows.
+    </div>
+  </footer>
+</template>
