a666cd904428ba866ece5573d9f72a5074d32819
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-26 20:41:24 +0200

    refactor: integrate AppCard component into ShowCard for streamlined display of show details and improved layout

--- src/components/ShowCard.vue
+++ src/components/ShowCard.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import RateBadge from '@/components/RateBadge.vue';
 import AppTag from '@/components/AppTag.vue';
+import AppCard from '@/components/AppCard.vue';
 import type { Show } from '@/types/shows';
 
 const props = defineProps<{ show: Show }>();
@@ -8,43 +9,22 @@ const props = defineProps<{ show: Show }>();
 
 <template>
   <RouterLink :to="`/show/${props.show.id}`">
-    <article class="show-card group">
-      <div class="overflow-hidden">
-        <img
-          :src="props.show.image?.medium"
-          :alt="props.show.name"
-          loading="lazy"
-          class="show-image"
-        />
-      </div>
-      <div class="p-3">
-        <h3 class="text-sm sm:text-base md:text-lg font-medium mb-4 truncate">
-          {{ props.show.name }}
-        </h3>
+    <AppCard
+      :image="props.show.image?.medium"
+      :imageAlt="props.show.name"
+      :title="props.show.name"
+      :hoverAnimation="true"
+      size="large"
+    >
+      <template #subtitle>
         <div class="hidden sm:flex gap-1 flex-wrap">
           <AppTag :label="props.show.status" />
           <AppTag :label="props.show.language" />
         </div>
-      </div>
-      <div class="absolute top-1 right-1">
+      </template>
+      <template #overlay>
         <RateBadge :rate="props.show.rating.average" />
-      </div>
-    </article>
+      </template>
+    </AppCard>
   </RouterLink>
 </template>
-
-<style scoped>
-@reference "@/style.css";
-
-.show-card {
-  @apply relative rounded-xl overflow-hidden 
-  mx-auto flex flex-col bg-white shadow-lg 
-  duration-300 hover:-translate-y-1 
-  w-36 sm:w-44 md:w-52
-  hover:shadow-2xl;
-}
-
-.show-image {
-  @apply transition-transform duration-300 group-hover:scale-105;
-}
-</style>
