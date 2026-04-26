f5c11f57b7dcf146d4f5ed554ecd8189989609da
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-26 21:23:59 +0200

    feat: add TVIcon component for SVG representation of a television icon

---
+++ src/components/icons/TVIcon.vue
@@ -0,0 +1,14 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="24"
+    height="24"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+    aria-hidden="true"
+  >
+    <path
+      d="M21 3H3a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h5v2H6v2h12v-2h-2v-2h5a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2zm0 14H3V5h18v12z"
+    />
+  </svg>
+</template>
