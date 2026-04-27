46c6ad28c7291e6a765a0a5fc1b9b0f9af483164
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-27 16:28:52 +0200

    feat: add AppLoading component with loading indicator and accessibility features

---
+++ src/components/AppLoading.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import RingIcon from '@/components/icons/RingIcon.vue';
+</script>
+<template>
+  <div class="w-full flex items-center justify-center p-2">
+    <p class="sr-only" aria-live="polite">Loading ...</p>
+    <RingIcon />
+  </div>
+</template>
