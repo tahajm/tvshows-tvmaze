ab712892049a4215f67fcb81090f34b05db4031e
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-27 16:29:48 +0200

    feat: add searchShows function to API for querying shows by search term

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,6 +2,8 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL ?? 'https://api.tvmaze.com';
 
 export const API = {
   shows: `${BASE_URL}/shows`,
+  searchShows: (searchTerm: string) =>
+    `${BASE_URL}/search/shows?q=${searchTerm}`,
   show: (id: number | string) =>
     `${BASE_URL}/shows/${id}?embed[]=images&embed[]=cast&embed[]=episodes`,
 } as const;
