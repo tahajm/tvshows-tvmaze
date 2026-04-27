fa033e43ddfe88649b242e028562d7a04f566064
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-27 18:11:05 +0200

    fix: correct import path for AppFooter component and add AppFooter.vue with footer template

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import AppHeader from '@/components/AppHeader.vue';
-import AppFooter from '@/components/AooFooter.vue';
+import AppFooter from '@/components/AppFooter.vue';
 </script>
 
 <template>
