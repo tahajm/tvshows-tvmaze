0fa9fdaa6f5e2d524b6906b4cfdfc29ab0af39fd
Taha Jamshidi <tahajamshidi58@gmail.com>
2026-04-27 18:11:27 +0200

    feat: add NotFound component and update router to handle 404 errors

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from 'vue-router';
 import ShowListView from '@/views/ShowsList.vue';
 import ShowDetail from '@/views/ShowDetail.vue';
 import SearchShow from '@/views/SearchShow.vue';
+import NotFound from '@/views/NotFound.vue';
 
 const routes = [
   { path: '/', component: ShowListView },
   { path: '/search', component: SearchShow },
   { path: '/show/:id', component: ShowDetail, props: true },
+  { path: '/:pathMatch(.*)*', component: NotFound },
 ];
 
 export const router = createRouter({
---
+++ src/views/NotFound.vue
@@ -0,0 +1,13 @@
+<template>
+  <main
+    class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-24 flex flex-col items-center gap-6 text-center"
+  >
+    <h2 class="text-2xl font-semibold">404 Page not found</h2>
+    <RouterLink
+      to="/"
+      class="mt-2 px-6 py-2.5 rounded-xl bg-primary text-white"
+    >
+      Back to home
+    </RouterLink>
+  </main>
+</template>
